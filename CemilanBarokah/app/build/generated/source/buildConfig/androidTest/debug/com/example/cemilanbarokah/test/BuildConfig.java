82c8b7cbc780af5772dd5b0467dfe32c18b72c69
rezariski <82254440+rezariski@users.noreply.github.com>
2021-09-25 17:40:35 +0700

    Minggu ke 3
    Minggu ke 3

---
+++ CemilanBarokah/app/build/generated/source/buildConfig/androidTest/debug/com/example/cemilanbarokah/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.cemilanbarokah.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.cemilanbarokah.test";
+  public static final String BUILD_TYPE = "debug";
+}
