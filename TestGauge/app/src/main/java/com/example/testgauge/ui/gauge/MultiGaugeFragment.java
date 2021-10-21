680806fb5b7809edf6c712722c0a13fe584bf641
rezariski <82254440+rezariski@users.noreply.github.com>
2021-10-21 13:39:15 +0700

    E32201076_Muhammad Yusuf Reyzha Rizky Alatas
    E32201076_Muhammad Yusuf Reyzha Rizky Alatas

--- TestGauge/app/src/main/java/com/example/testgauge/ui/gauge/MultiGaugeFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example..ui.gauge;
-
-import android.app.Activity;
-
-public class MultiGaugeFragment extends Activity {
-}
--- TestGauge/library/src/main/java/com/example/library/contract/ValueFormatter.java
+++
@@ -1,5 +0,0 @@
-package com.example.library.contract;
-
-public interface ValueFormatter {
-    String getFormattedValue(double value);
-}
